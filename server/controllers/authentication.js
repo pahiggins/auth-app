18d661eb3f8899706b9cff7e51f979678696c913
Peter Higgins <pah@cenabo.com>
2018-03-15 10:01:21 +0000

    Authorizing users

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,8 +1,25 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next) {
+  // User has already had their email and password auth'd
+  // We just need to give them a token
+  res.send({ token: tokenForUser(req.user) });
+}
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide an email and password.' })
+  }
 
   // See if a user with the given email already exists
   User.findOne({ email }, function(err, existingUser) {
@@ -23,7 +40,7 @@ exports.signup = function(req, res, next) {
       if (err) { return next(err); }
 
       // Respond to request indicating the user was created
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     });
   })
 }
